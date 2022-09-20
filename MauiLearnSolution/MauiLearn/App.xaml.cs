a706fb80ea04845b6d565cd3993a311cad4617e6
Michael <Benavides.michael.munoz@gmail.com>
2022-09-20 20:57:02 +0200

    blank project

---
+++ MauiLearnSolution/MauiLearn/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiLearn
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
